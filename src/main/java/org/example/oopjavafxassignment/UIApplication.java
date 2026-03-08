b505a0470cbb8b1eaefa2bc9e8dbdca4cbde63be
WJijon223 <williamjijon223@gmail.com>
2026-03-08 03:40:15 -0400

    adjusted window sizes for user interfaces

--- src/main/java/org/example/oopjavafxassignment/UIApplication.java
+++ src/main/java/org/example/oopjavafxassignment/UIApplication.java
@@ -2,7 +2,9 @@ package org.example.oopjavafxassignment;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +13,11 @@ public class UIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UIApplication.class.getResource("user-interface.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth(), screenBounds.getHeight());
+        stage.setTitle("CSC325 OOP/JavaFX Assignment | William Jijon");
         stage.setScene(scene);
         stage.show();
     }
